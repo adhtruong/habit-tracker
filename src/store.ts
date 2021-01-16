d09f3f6a45a32e8a654cb5c1fb83e2298b19e8af
Andrew Truong <adh.truong@gmail.com>
2021-01-16 17:23:54 +0000

    Add state to localStorage

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,32 @@
+import throttle from "lodash/throttle";
 import { createStore } from "redux";
 
-import rootReducer from "./features";
+import rootReducer, { RootState } from "./features";
 
-const store = createStore(rootReducer);
+const _persistantState = "persitantState";
+
+const saveToLocalStorage = (state: RootState) => {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem(_persistantState, serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serialisedState = localStorage.getItem(_persistantState);
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+};
+
+const store = createStore(rootReducer, loadFromLocalStorage());
+
+store.subscribe(throttle(() => saveToLocalStorage(store.getState()), 1000));
 
 export default store;
